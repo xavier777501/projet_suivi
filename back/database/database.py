535cd09916a257c681e763437c90e5089df0608f
xavier777501 <xavier7.com@gmail.com>
2026-01-05 16:11:45 +0100

    Correction automatique du driver MySQL

--- back/database/database.py
+++ back/database/database.py
@@ -5,10 +5,13 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 # Utilise DATABASE_URL de l'environnement (Render) ou l'adresse locale par défaut
-SQLALCHEMY_DATABASE_URL = os.getenv(
-    "DATABASE_URL", 
-    "mysql+pymysql://root:@localhost/suiviprojet"
-)
+url = os.getenv("DATABASE_URL", "mysql+pymysql://root:@localhost/suiviprojet")
+
+# Si l'URL commence par 'mysql://' (donné par TiDB), on la transforme en 'mysql+pymysql://'
+if url and url.startswith("mysql://"):
+    url = url.replace("mysql://", "mysql+pymysql://", 1)
+
+SQLALCHEMY_DATABASE_URL = url
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
